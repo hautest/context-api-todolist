b56b048444b759b8aee748227fd761ed329ecc81
LeeSeungHun <dos07008@naver.com>
2022-09-27 01:14:34 +0900

    Provider 적용하기

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { TodoProvider } from "./todoContext";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoProvider>
+      <App />
+    </TodoProvider>
   </React.StrictMode>
 );
